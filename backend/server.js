b017481f62b5517d1a6b4ca5e2dc233730ddbf9a
Thanuj <thanujdevil@gmail.com>
2024-06-28 01:41:50 +0530

    fixes

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,13 @@ const cors = require("cors")
 const bodyParser = require("body-parser")
 const config = require("./config");
 
+const app = express()
 
 const corsOptions = {
     origin: 'https://simple-store-cms.vercel.app',
     optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
-
-
-const app = express()
 
 app.use(express.json());
 app.use(cors(corsOptions));
